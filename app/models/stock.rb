533a6f8e108ebcf4d8b6831777969352b64f0174
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2016-03-10 17:57:27 +0100

    user_stock model created and validation added

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ActiveRecord::Base
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
@@ -18,4 +21,6 @@ class Stock < ActiveRecord::Base
     return "#{opening_price} (Opening)" if opening_price
     'Unavailable'
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+
   resources :welcomes
   devise_for :users
+  resources :user_stocks, except: [:show, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'welcomes#index'
-  get 'my_portfolio', to:"users#my_portfolio"
-  get 'search_stocks', to:"stocks#search"
+  root 'welcomes#index'
+  get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
