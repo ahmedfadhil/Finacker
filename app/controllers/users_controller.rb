2770bddd3412178f417cc6d072d1d55d5a89608f
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2016-03-15 01:01:20 +0100

    friends list created and added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
     @user_stocks = current_user.stocks
     @user = current_user
   end
+
   def my_friends
+    @friendships = current_user.friends
   end
   # before_action :set_welcome, only: [:show, :edit, :update, :destroy]
   #
